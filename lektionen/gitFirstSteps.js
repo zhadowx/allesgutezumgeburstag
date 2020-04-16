1ae21618a846852cf05f50f99b1f7c4ef51d91fe
zhadowx <nlorax@gmail.com>
2020-04-15 20:38:27 -0400

    github steps and new beersong :p

---
+++ lektionen/gitFirstSteps.js
@@ -0,0 +1,8 @@
+//for the first time
+git clone https://github.com/zhadowx/allesgutezumgeburstag.git
+
+//continously
+git add -A
+git status //check all green
+git commit -m "commit message" //pequena descripcion del cambio
+git push
