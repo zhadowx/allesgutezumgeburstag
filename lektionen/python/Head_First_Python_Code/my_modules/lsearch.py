c0bdf158195708d498d195b5e41782ba86cb7616
zhadowx <nlorax@gmail.com>
2020-04-23 09:15:40 -0400

    PEP 8 complience

--- lektionen/python/Head_First_Python_Code/my_modules/lsearch.py
+++ lektionen/python/Head_First_Python_Code/my_modules/lsearch.py
@@ -1,11 +1,9 @@
-def search_for_vowels(phrase:str) -> set:
+def search_for_vowels(phrase: str) -> set:
     """Return any vowels found in a supplied phrase."""
     vowels = set('aeiou')
-    return vowels.intersection(set(phrase)) 
+    return vowels.intersection(set(phrase))
 
-def search_for_letters(phrase:str, letters:str = 'aeiou') -> set:
+
+def search_for_letters(phrase: str, letters: str = 'aeiou') -> set:
     """Return a set of the 'letters' found in 'Phrase'."""
     return set(letters).intersection(set(phrase))
-
-#print(search_for_letters(letters='xyz', phrase='galaxy')) 
-#When you refer to arguments by their name, positional ordering no longer applies.
