ef3e9b4c976d77ed8f6b3ff2b62b589e92a8d206
zhadowx <nlorax@gmail.com>
2020-04-23 13:37:50 -0400

    First steps on web apps, html and css

---
+++ lektionen/python/Head_First_Python_Code/webapp/lsearchweb.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+from lsearch import search_for_letters
+
+app = Flask (__name__)
+
+@app.route('/')
+def hello() -> str:
+  return 'Hello world from Flask!'
+
+
+@app.route('/search_for')
+def do_search() -> str:
+  return str(search_for_letters('life, the universe, and everything!', 'eiru,!'))
+
+
+@app.route('/entry')
+def entry_page() -> 'html':
+  return render_template('entry.html', the_title = "Welcome to the Jungle!!")
+
+app.run()
