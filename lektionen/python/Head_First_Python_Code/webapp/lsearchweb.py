d2bbd99d6d1d3f419ff5d3777754494ade9193d5
zhadowx <nlorax@gmail.com>
2020-04-24 14:12:06 -0400

    Webapp extras and ready to go!

--- lektionen/python/Head_First_Python_Code/webapp/lsearchweb.py
+++ lektionen/python/Head_First_Python_Code/webapp/lsearchweb.py
@@ -1,20 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from lsearch import search_for_letters
 
 app = Flask (__name__)
 
+@app.route('/search_for', methods=['POST'])
+def do_search() -> 'html':
+  phrase = request.form['phrase']
+  letters = request.form['letters']
+  results = str(sorted(search_for_letters(phrase, letters)))
+  title = 'Here are your results'
+  return render_template('results.html', the_title=title, the_phrase=phrase, the_letters=letters, the_results=results)
+
+
 @app.route('/')
-def hello() -> str:
-  return 'Hello world from Flask!'
-
-
-@app.route('/search_for')
-def do_search() -> str:
-  return str(search_for_letters('life, the universe, and everything!', 'eiru,!'))
-
-
 @app.route('/entry')
 def entry_page() -> 'html':
-  return render_template('entry.html', the_title = "Welcome to the Jungle!!")
+  return render_template('entry.html', the_title = "Welcome to search for letters")
 
-app.run()
+
+if __name__=='__main__':
+  app.run(debug=True)
