89551159fd296862459b19485438f75dedb69443
zhadowx <nlorax@gmail.com>
2020-04-28 20:52:07 -0400

    Connecting SQL with Python

--- lektionen/python/Head_First_Python_Code/trials.py
+++ lektionen/python/Head_First_Python_Code/trials.py
@@ -1,3 +1,22 @@
-import html
+import mysql.connector
 
-print(html.unescape("&#208;&epsilon;&tau;&#114;&ouml;&igrave;&tau;"))
+dbconfig = { 'host': '127.0.0.1',
+             'user': 'lsearch',
+             'password': '1234567',
+             'database': 'lsearchlogDB', }
+
+conn = mysql.connector.connect(**dbconfig)
+cursor = conn.cursor()
+
+_SQL = """insert into log
+             (phrase, letters, ip, browser_string, results)
+             values
+             (%s, %s, %s, %s, %s)"""
+
+cursor.execute(_SQL, ('palomo', 'o', '127.0.0.1', 'Chrome', "{'o'}"))
+conn.commit()
+
+_SQL = """select * from log"""
+cursor.execute(_SQL)
+for row in cursor.fetchall():
+  print(row)
