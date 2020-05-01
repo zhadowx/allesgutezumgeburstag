fcd9b57f65f374f1adab79a8e6c11fb9956c20eb
zhadowx <nlorax@gmail.com>
2020-05-01 19:15:17 -0400

    Using classes to create a context manager for webapp

--- lektionen/python/Head_First_Python_Code/trials.py
+++ lektionen/python/Head_First_Python_Code/trials.py
@@ -1,22 +1,36 @@
 import mysql.connector
 
-dbconfig = { 'host': '127.0.0.1',
-             'user': 'lsearch',
-             'password': '1234567',
-             'database': 'lsearchlogDB', }
+class UseDatabase:
+  def __init__(self, config: dict) -> None:
+    self.configuration =  config
 
-conn = mysql.connector.connect(**dbconfig)
-cursor = conn.cursor()
 
-_SQL = """insert into log
-             (phrase, letters, ip, browser_string, results)
-             values
-             (%s, %s, %s, %s, %s)"""
+  def __enter__(self) -> 'cursor':
+    self.conn = mysql.connector.connect(**self.configuration)
+    self.cursor = self.conn.cursor()
+    return self.cursor
 
-cursor.execute(_SQL, ('palomo', 'o', '127.0.0.1', 'Chrome', "{'o'}"))
-conn.commit()
 
-_SQL = """select * from log"""
-cursor.execute(_SQL)
-for row in cursor.fetchall():
-  print(row)
+  def __exit__(self, exc_type, exc_value, exc_trace) -> None:
+    self.conn.commit()
+    self.cursor.close()
+    self.conn.close()
+    
+
+ # dbconfig = { 'host': '127.0.0.1',
+ #               'user': 'lsearch',
+ #               'password': '1234567',
+ #               'database': 'lsearchlogDB', }
+
+               
+ #  with UseDatabase(dbconfig) as cursor:
+ #    _SQL = """insert into log
+ #               (phrase, letters, ip, browser_string, results)
+ #               values
+ #               (%s, %s, %s, %s, %s)"""
+
+ #    cursor.execute(_SQL, (req.form['phrase'],
+ #                          req.form['letters'],
+ #                          req.remote_addr,
+ #                          req.user_agent.browser,
+ #                          res, ))
---
+++ lektionen/python/Head_First_Python_Code/webapp/lsearchweb_4.py
@@ -0,0 +1,68 @@
+from flask import Flask, render_template, request, escape
+from lsearch import search_for_letters
+from DBcm import UseDatabase
+
+app = Flask (__name__)
+
+app.config['dbconfig'] = { 'host': '127.0.0.1',
+                           'user': 'lsearch',
+                           'password': '1234567',
+                           'database': 'lsearchlogDB', }
+
+
+def log_request(req: 'flask_request', res: str) -> None:
+  """Log details of the web request and the results."""
+
+  with UseDatabase(app.config['dbconfig']) as cursor:
+    _SQL = """insert into log
+               (phrase, letters, ip, browser_string, results)
+               values
+               (%s, %s, %s, %s, %s)"""
+
+    cursor.execute(_SQL, (req.form['phrase'],
+                          req.form['letters'],
+                          req.remote_addr,
+                          req.user_agent.browser,
+                          res, ))
+
+
+@app.route('/search_for', methods=['POST'])
+def do_search() -> 'html':
+  """Display the contents of the search_for_letters function as a HTML page"""
+  phrase = request.form['phrase']
+  letters = request.form['letters']
+  results = str(sorted(search_for_letters(phrase, letters)))
+  title = 'Here are your results'
+  log_request(request, results)
+  return render_template('results.html', 
+                         the_title=title, 
+                         the_phrase=phrase, 
+                         the_letters=letters, 
+                         the_results=results)
+
+
+@app.route('/')
+@app.route('/entry')
+def entry_page() -> 'html':
+  """Renders the page to input the data into the search_for_letters function"""
+  return render_template('entry.html', 
+                         the_title = "Welcome to search for letters")
+
+
+@app.route('/viewlog')
+def view_log() -> 'html':
+  """Display the contents of the log file as a HTML table."""
+  with UseDatabase(app.config['dbconfig']) as cursor:
+    _SQL = """select phrase, letters, ip, browser_string, results 
+              from log"""
+    cursor.execute(_SQL)
+    contents = cursor.fetchall()
+  titles = ('Phrase', 'Letters', 'Remote_addr', 'User_agent', 'Results')
+  return render_template('viewlog.html', 
+                         the_title='View Log', 
+                         the_row_titles=titles, 
+                         the_data=contents)
+
+
+if __name__=='__main__':
+  app.run(debug=True)
